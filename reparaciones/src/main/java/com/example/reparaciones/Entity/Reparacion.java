6a481c00abd36b466787dc1d50b0e397a52d0b19
Matias <matias.aravena.f@usach.cl>
2024-05-21 21:50:59 -0400

    listos todos los servicios

--- reparaciones/src/main/java/com/example/reparaciones/Entity/Reparacion.java
+++ reparaciones/src/main/java/com/example/reparaciones/Entity/Reparacion.java
@@ -2,10 +2,16 @@ package com.example.reparaciones.Entity;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.sql.Time;
 
 @Entity
 @Data
@@ -13,5 +19,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Reparacion {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String patente;
+    private Date fechaIng;
+    private Time horaIng;
+    private Integer montoTotalReparaciones;
+    private Integer recargos;
+    private Integer descuentos;
+    private Integer iva;
+    private Integer total;
+    private Date fechaSal;
+    private Time horaSal;
+    private Date fechaRet;
+    private Time horaRet;
 }
--- reparaciones/src/main/java/com/example/reparaciones/Repository/ReparacionesRepository.java
+++ reparaciones/src/main/java/com/example/reparaciones/Repository/ReparacionesRepository.java
@@ -3,5 +3,6 @@ package com.example.reparaciones.Repository;
 import com.example.reparaciones.Entity.Reparacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ReparacionesRepository extends JpaRepository<Reparacion, String> {
 }
